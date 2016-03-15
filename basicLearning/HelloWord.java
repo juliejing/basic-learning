6883c9f4c033560743f575666b0ffeffdb1da57f
julie <531551816@qq.com>
2016-03-14 23:26:58 -0400

    commit the first time

---
+++ basicLearning/HelloWord.java
@@ -0,0 +1,12 @@
+package basicLearning;
+
+import java.util.Date;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("hello world! "+ new Date() + "-----liujing");
+
+	}
+
+}
